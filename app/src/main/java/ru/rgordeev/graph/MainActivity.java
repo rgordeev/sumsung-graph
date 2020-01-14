0a86e107f0e901f7812db5f3a0e2bd5ed2a15613
rgordeev <roma.gordeev@gmail.com>
2020-01-14 19:43:08 +0300

    animations

--- app/src/main/java/ru/rgordeev/graph/MainActivity.java
+++ app/src/main/java/ru/rgordeev/graph/MainActivity.java
@@ -3,12 +3,28 @@ package ru.rgordeev.graph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean fade = false;
+
+    public void fade(View view) {
+        ImageView imageView = (ImageView) view;
+        imageView.animate()
+                .alpha(fade ? 1 : 0)
+                .rotationBy(1800)
+                .translationXBy(1000)
+                .setDuration(3000);
+        fade = !fade;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.animate().translationX(-1000);
     }
 }
